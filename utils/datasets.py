b7d417d8bd482b131893ffcba33b0cc6c22d986a
Chrysa Pratikaki <chrisaprat@gmail.com>
2025-05-28 13:55:01 +0300

    Update datasets.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -95,7 +95,11 @@ class DataAugmentationForMultiMAE(object):
             if task in ['b1','b2','b3','s1']:
                 img = torch.Tensor(np.array(task_dict[task]) / 2 ** 16)
                 img = img.unsqueeze(0)  # 1 x H x W
-            elif task in ['rgb','s2','depth']:
+            elif task == 'depth':
+                img = TF.to_tensor(task_dict[task])  # shape: [1, H, W]
+                img = TF.normalize(img, mean=[0.5], std=[0.5])  # or whatever is appropriate
+
+            elif task in ['rgb','s2']:
                 img = TF.to_tensor(task_dict[task])
                 img = TF.normalize(img, mean=self.rgb_mean, std=self.rgb_std)
             elif task in ['semseg', 'semseg_coco']:
