7834f3122bfbf4d3b3f67287fbcd5de83125a695
Chrysa Pratikaki <chrisaprat@gmail.com>
2025-05-28 14:03:36 +0300

    Update run_pretraining_multimae.py

--- run_pretraining_multimae.py
+++ run_pretraining_multimae.py
@@ -520,11 +520,11 @@ def train_one_epoch(model: torch.nn.Module, data_loader: Iterable, tasks_loss_fn
         }
 
         # Truncated depth standardization
-        if standardize_depth and 'depth' in tasks_dict:
-            # Flatten depth and remove bottom and top 10% of values
-            trunc_depth = torch.sort(rearrange(tasks_dict['depth'], 'b c h w -> b (c h w)'), dim=1)[0]
-            trunc_depth = trunc_depth[:,int(0.1 * trunc_depth.shape[1]): int(0.9 * trunc_depth.shape[1])]
-            tasks_dict['depth'] = (tasks_dict['depth'] - trunc_depth.mean(dim=1)[:,None,None,None]) / torch.sqrt(trunc_depth.var(dim=1)[:,None,None,None] + 1e-6)
+        # if standardize_depth and 'depth' in tasks_dict:
+        #     # Flatten depth and remove bottom and top 10% of values
+        #     trunc_depth = torch.sort(rearrange(tasks_dict['depth'], 'b c h w -> b (c h w)'), dim=1)[0]
+        #     trunc_depth = trunc_depth[:,int(0.1 * trunc_depth.shape[1]): int(0.9 * trunc_depth.shape[1])]
+        #     tasks_dict['depth'] = (tasks_dict['depth'] - trunc_depth.mean(dim=1)[:,None,None,None]) / torch.sqrt(trunc_depth.var(dim=1)[:,None,None,None] + 1e-6)
 
         input_dict = {
             task: tensor
